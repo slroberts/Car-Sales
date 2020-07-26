b8d903569feb76f8548edf1661a88f853b0df196
slroberts <shomariroberts@gmail.comclear>
2020-07-26 18:40:53 -0400

    completed MVP

---
+++ src/reducers/carReducer.js
@@ -0,0 +1,55 @@
+import {ADD_FEATURE, REMOVE_FEATURE} from "../actions/featureActions";
+
+const initialState = {
+  car: {
+    price: 26395,
+    name: "2019 Ford Mustang",
+    image:
+      "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
+    features: [],
+  },
+  additionalPrice: 0,
+  additionalFeatures: [
+    {id: 1, name: "V-6 engine", price: 1500},
+    {id: 2, name: "Racing detail package", price: 1500},
+    {id: 3, name: "Premium sound system", price: 500},
+    {id: 4, name: "Rear spoiler", price: 250},
+  ],
+};
+
+export const carReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_FEATURE: {
+      return {
+        ...state,
+        car: {
+          ...state.car,
+          price: state.car.price + action.payload.price,
+          features: [...state.car.features, action.payload],
+        },
+        // additionalPrice: state.additionalPrice + action.payload.price,
+        additionalFeatures: state.additionalFeatures.filter(
+          (feature) => feature.id !== action.payload.id
+        ),
+      };
+    }
+
+    case REMOVE_FEATURE: {
+      return {
+        ...state,
+        car: {
+          ...state.car,
+          price: state.car.price - action.payload.price,
+          features: state.car.features.filter(
+            (feature) => feature.id !== action.payload.id
+          ),
+        },
+        // additionalPrice: state.additionalPrice - action.payload.price,
+        additionalFeatures: [...state.additionalFeatures, action.payload],
+      };
+    }
+
+    default:
+      return state;
+  }
+};
